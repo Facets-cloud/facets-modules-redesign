f9a5ebcd489ffdf84331ab3aa79dac0bd79bfc48
srikxcipher <pandasrikant365@gmail.com>
2025-09-04 14:53:14 +0530

    added...new private services connection attributes

--- network-gcp-vpc/outputs.tf
+++ network-gcp-vpc/outputs.tf
@@ -1,23 +1,29 @@
 locals {
   output_attributes = {
-    zones                               = local.selected_zones
-    region                              = var.instance.spec.region
-    vpc_id                              = google_compute_network.main.id
-    vpc_name                            = google_compute_network.main.name
-    project_id                          = var.inputs.cloud_account.attributes.project
-    router_ids                          = values(google_compute_router.main)[*].id
-    vpc_self_link                       = google_compute_network.main.self_link
-    nat_gateway_ids                     = values(google_compute_router_nat.main)[*].id
-    firewall_rule_ids                   = compact(concat(try([google_compute_firewall.allow_internal[0].id], []), try([google_compute_firewall.allow_ssh[0].id], []), try([google_compute_firewall.allow_http[0].id], []), try([google_compute_firewall.allow_https[0].id], []), try([google_compute_firewall.allow_icmp[0].id], [])))
-    public_subnet_ids                   = values(google_compute_subnetwork.public)[*].id
-    private_subnet_ids                  = values(google_compute_subnetwork.private)[*].id
-    database_subnet_ids                 = values(google_compute_subnetwork.database)[*].id
-    public_subnet_cidrs                 = [for subnet in local.public_subnets : subnet.cidr_block]
-    private_subnet_cidrs                = [for subnet in local.private_subnets : subnet.cidr_block]
-    database_subnet_cidrs               = [for subnet in local.database_subnets : subnet.cidr_block]
+    zones                 = local.selected_zones
+    region                = var.instance.spec.region
+    vpc_id                = google_compute_network.main.id
+    vpc_name              = google_compute_network.main.name
+    project_id            = var.inputs.cloud_account.attributes.project
+    router_ids            = values(google_compute_router.main)[*].id
+    vpc_self_link         = google_compute_network.main.self_link
+    nat_gateway_ids       = values(google_compute_router_nat.main)[*].id
+    firewall_rule_ids     = compact(concat(try([google_compute_firewall.allow_internal[0].id], []), try([google_compute_firewall.allow_ssh[0].id], []), try([google_compute_firewall.allow_http[0].id], []), try([google_compute_firewall.allow_https[0].id], []), try([google_compute_firewall.allow_icmp[0].id], [])))
+    public_subnet_ids     = values(google_compute_subnetwork.public)[*].id
+    private_subnet_ids    = values(google_compute_subnetwork.private)[*].id
+    database_subnet_ids   = values(google_compute_subnetwork.database)[*].id
+    public_subnet_cidrs   = [for subnet in local.public_subnets : subnet.cidr_block]
+    private_subnet_cidrs  = [for subnet in local.private_subnets : subnet.cidr_block]
+    database_subnet_cidrs = [for subnet in local.database_subnets : subnet.cidr_block]
+
+    # Private Services Connection Attributes
     private_services_connection_status  = lookup(local.private_services_connection, "enable", true)
     private_services_peering_connection = lookup(local.private_services_connection, "enable", true) ? google_service_networking_connection.private_services_connection[0].network : null
+    private_services_range_name         = lookup(local.private_services_connection, "enable", true) ? google_compute_global_address.private_services_range[0].name : null
+    private_services_range_id           = lookup(local.private_services_connection, "enable", true) ? google_compute_global_address.private_services_range[0].id : null
+    private_services_connection_id      = lookup(local.private_services_connection, "enable", true) ? google_service_networking_connection.private_services_connection[0].id : null
+    private_services_range_address      = lookup(local.private_services_connection, "enable", true) ? google_compute_global_address.private_services_range[0].address : null
   }
   output_interfaces = {
   }
-}
+} 
