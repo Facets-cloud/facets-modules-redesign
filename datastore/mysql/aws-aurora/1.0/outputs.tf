59d19d31a6bc1f9b4c112532e2072566d9ea8596
srikxcipher <pandasrikant365@gmail.com>
2025-10-30 12:46:43 +0530

    backup : commented out output_attributes across all outputs.tf files.

--- datastore/mysql/aws-aurora/1.0/outputs.tf
+++ datastore/mysql/aws-aurora/1.0/outputs.tf
@@ -1,76 +1,75 @@
 locals {
-  output_attributes = {
-    identifier                   = aws_rds_cluster.aurora.cluster_identifier
-    arn                          = aws_rds_cluster.aurora.arn
-    writer_endpoint              = aws_rds_cluster.aurora.endpoint
-    reader_endpoint              = aws_rds_cluster.aurora.reader_endpoint
-    port                         = aws_rds_cluster.aurora.port
-    database_name                = aws_rds_cluster.aurora.database_name
-    master_username              = aws_rds_cluster.aurora.master_username
-    master_password              = sensitive(local.is_import ? "<imported-password-not-available>" : (local.restore_from_backup ? var.instance.spec.restore_config.master_password : local.master_password))
-    engine_version               = aws_rds_cluster.aurora.engine_version
-    backup_retention_period      = aws_rds_cluster.aurora.backup_retention_period
-    subnet_group_name            = local.is_import ? aws_rds_cluster.aurora.db_subnet_group_name : aws_db_subnet_group.aurora[0].name
-    security_group_id            = local.is_import ? null : aws_security_group.aurora[0].id
-    # availability_zones           = aws_rds_cluster.aurora.availability_zones
-    # writer_instance_id           = length(aws_rds_cluster_instance.aurora_writer) > 0 ? aws_rds_cluster_instance.aurora_writer[0].identifier : null
-    # reader_instance_ids          = aws_rds_cluster_instance.aurora_readers[*].identifier
-    # preferred_backup_window      = aws_rds_cluster.aurora.preferred_backup_window
-    # preferred_maintenance_window = aws_rds_cluster.aurora.preferred_maintenance_window
-    is_restored_from_snapshot    = local.restore_from_backup
-    is_imported                  = local.is_import
-    secrets                      = ["master_password"]
-  }
+  # output_attributes = {
+  #   identifier              = aws_rds_cluster.aurora.cluster_identifier
+  #   arn                     = aws_rds_cluster.aurora.arn
+  #   writer_endpoint         = aws_rds_cluster.aurora.endpoint
+  #   reader_endpoint         = aws_rds_cluster.aurora.reader_endpoint
+  #   port                    = aws_rds_cluster.aurora.port
+  #   database_name           = aws_rds_cluster.aurora.database_name
+  #   master_username         = aws_rds_cluster.aurora.master_username
+  #   master_password         = sensitive(local.is_import ? "<imported-password-not-available>" : (local.restore_from_backup ? var.instance.spec.restore_config.master_password : local.master_password))
+  #   engine_version          = aws_rds_cluster.aurora.engine_version
+  #   backup_retention_period = aws_rds_cluster.aurora.backup_retention_period
+  #   subnet_group_name       = local.is_import ? aws_rds_cluster.aurora.db_subnet_group_name : aws_db_subnet_group.aurora[0].name
+  #   security_group_id       = local.is_import ? null : aws_security_group.aurora[0].id
+  #   # availability_zones           = aws_rds_cluster.aurora.availability_zones
+  #   # writer_instance_id           = length(aws_rds_cluster_instance.aurora_writer) > 0 ? aws_rds_cluster_instance.aurora_writer[0].identifier : null
+  #   # reader_instance_ids          = aws_rds_cluster_instance.aurora_readers[*].identifier
+  #   # preferred_backup_window      = aws_rds_cluster.aurora.preferred_backup_window
+  #   # preferred_maintenance_window = aws_rds_cluster.aurora.preferred_maintenance_window
+  #   is_restored_from_snapshot = local.restore_from_backup
+  #   is_imported               = local.is_import
+  #   secrets                   = ["master_password"]
+  # }
   output_interfaces = {
     reader = {
-      username           = aws_rds_cluster.aurora.master_username
-      port               = tostring(aws_rds_cluster.aurora.port)
-      password           = local.is_import ? "<imported-password-not-available>" : (
+      host     = aws_rds_cluster.aurora.reader_endpoint
+      username = aws_rds_cluster.aurora.master_username
+      port     = tostring(aws_rds_cluster.aurora.port)
+      password = local.is_import ? "<imported-password-not-available>" : (
         local.restore_from_backup ? var.instance.spec.restore_config.master_password : local.master_password
       )
-      database           = aws_rds_cluster.aurora.database_name
-      connection_string  = local.is_import ? [
-        format(
-          "mysql://%s:<password>@%s:%d/%s",
-          aws_rds_cluster.aurora.master_username,
-          aws_rds_cluster.aurora.reader_endpoint,
-          aws_rds_cluster.aurora.port,
-          coalesce(aws_rds_cluster.aurora.database_name, "")
-        )
-      ] : [
-        format(
-          "mysql://%s:%s@%s:%d/%s",
-          aws_rds_cluster.aurora.master_username,
-          local.restore_from_backup ? var.instance.spec.restore_config.master_password : local.master_password,
-          aws_rds_cluster.aurora.reader_endpoint,
-          aws_rds_cluster.aurora.port,
-          aws_rds_cluster.aurora.database_name
-          )
-        ]
-      }
+      database = aws_rds_cluster.aurora.database_name
+
+      connection_string = local.is_import ? format(
+        "mysql://%s:<password>@%s:%d/%s",
+        aws_rds_cluster.aurora.master_username,
+        aws_rds_cluster.aurora.reader_endpoint,
+        aws_rds_cluster.aurora.port,
+        coalesce(aws_rds_cluster.aurora.database_name, "")
+        ) : format(
+        "mysql://%s:%s@%s:%d/%s",
+        aws_rds_cluster.aurora.master_username,
+        local.restore_from_backup ? var.instance.spec.restore_config.master_password : local.master_password,
+        aws_rds_cluster.aurora.reader_endpoint,
+        aws_rds_cluster.aurora.port,
+        aws_rds_cluster.aurora.database_name
+      )
+    }
+
     writer = {
-      host              = aws_rds_cluster.aurora.endpoint
-      port              = tostring(aws_rds_cluster.aurora.port)
-      username          = aws_rds_cluster.aurora.master_username
-      password          = local.is_import ? "<imported-password-not-available>" : (
+      host     = aws_rds_cluster.aurora.endpoint
+      port     = tostring(aws_rds_cluster.aurora.port)
+      username = aws_rds_cluster.aurora.master_username
+      password = local.is_import ? "<imported-password-not-available>" : (
         local.restore_from_backup ? var.instance.spec.restore_config.master_password : local.master_password
       )
-      database          = aws_rds_cluster.aurora.database_name
+      database = aws_rds_cluster.aurora.database_name
       connection_string = local.is_import ? format(
         "mysql://%s:<password>@%s:%d/%s",
         aws_rds_cluster.aurora.master_username,
         aws_rds_cluster.aurora.endpoint,
         aws_rds_cluster.aurora.port,
         coalesce(aws_rds_cluster.aurora.database_name, "")
-      ) : format(
+        ) : format(
         "mysql://%s:%s@%s:%d/%s",
         aws_rds_cluster.aurora.master_username,
         local.restore_from_backup ? var.instance.spec.restore_config.master_password : local.master_password,
         aws_rds_cluster.aurora.endpoint,
         aws_rds_cluster.aurora.port,
         aws_rds_cluster.aurora.database_name
-        )
-      }
+      )
+    }
     secrets = ["writer", "reader"]
   }
 }
--- datastore/mysql/aws-rds/1.0/outputs.tf
+++ datastore/mysql/aws-rds/1.0/outputs.tf
@@ -1,40 +1,71 @@
 locals {
-  output_attributes = {
-    identifier               = aws_db_instance.mysql.id
-    arn                      = aws_db_instance.mysql.arn
-    writer_endpoint          = aws_db_instance.mysql.endpoint
-    reader_endpoint          = [for r in aws_db_instance.read_replicas : r.endpoint]
-    port                     = aws_db_instance.mysql.port
-    database_name            = aws_db_instance.mysql.db_name
-    master_username          = aws_db_instance.mysql.username
-    master_password          = sensitive(local.is_db_instance_import ? "[IMPORTED-NOT-AVAILABLE]" : local.master_password)
-    engine_version           = aws_db_instance.mysql.engine_version
-    backup_retention_period  = aws_db_instance.mysql.backup_retention_period
-    subnet_group_name        = aws_db_instance.mysql.db_subnet_group_name
-    security_group_id        = local.actual_security_group_id
-    # vpc_security_group_ids  = aws_db_instance.mysql.vpc_security_group_ids
-    # security_group_source   = local.sg_source
-    # instance_class          = aws_db_instance.mysql.instance_class
-    # allocated_storage       = aws_db_instance.mysql.allocated_storage
-    # multi_az                = aws_db_instance.mysql.multi_az
-    is_restored_from_snapshot = local.is_restore_operation
-    is_imported              = local.is_db_instance_import
-    secrets                  = ["master_password"]
-  }
+  # output_attributes = {
+  #   identifier               = aws_db_instance.mysql.id
+  #   arn                      = aws_db_instance.mysql.arn
+  #   writer_endpoint          = aws_db_instance.mysql.endpoint
+  #   reader_endpoint          = [for r in aws_db_instance.read_replicas : r.endpoint]
+  #   port                     = aws_db_instance.mysql.port
+  #   database_name            = aws_db_instance.mysql.db_name
+  #   master_username          = aws_db_instance.mysql.username
+  #   master_password          = sensitive(local.is_db_instance_import ? "[IMPORTED-NOT-AVAILABLE]" : local.master_password)
+  #   engine_version           = aws_db_instance.mysql.engine_version
+  #   backup_retention_period  = aws_db_instance.mysql.backup_retention_period
+  #   subnet_group_name        = aws_db_instance.mysql.db_subnet_group_name
+  #   security_group_id        = local.actual_security_group_id
+  #   # vpc_security_group_ids  = aws_db_instance.mysql.vpc_security_group_ids
+  #   # security_group_source   = local.sg_source
+  #   # instance_class          = aws_db_instance.mysql.instance_class
+  #   # allocated_storage       = aws_db_instance.mysql.allocated_storage
+  #   # multi_az                = aws_db_instance.mysql.multi_az
+  #   is_restored_from_snapshot = local.is_restore_operation
+  #   is_imported              = local.is_db_instance_import
+  #   secrets                  = ["master_password"]
+  # }
   output_interfaces = {
     reader = sensitive({
-      username           = aws_db_instance.mysql.username
-      port               = aws_db_instance.mysql.port
-      password           = local.is_db_instance_import ? "[IMPORTED-NOT-AVAILABLE]" : local.master_password
-      database           = aws_db_instance.mysql.db_name
-      connection_string = local.is_db_instance_import ? [
-        for r in aws_db_instance.read_replicas : 
-        "mysql://${aws_db_instance.mysql.username}:[PASSWORD]@${r.address}:${r.port}/${aws_db_instance.mysql.db_name}"
-      ] : [
-        for r in aws_db_instance.read_replicas : 
-        "mysql://${aws_db_instance.mysql.username}:${local.master_password}@${r.address}:${r.port}/${aws_db_instance.mysql.db_name}"
-      ]
+      host     = length(aws_db_instance.read_replicas) > 0 ? aws_db_instance.read_replicas[0].address : aws_db_instance.mysql.address
+      username = aws_db_instance.mysql.username
+      port     = aws_db_instance.mysql.port
+      password = local.is_db_instance_import ? "[IMPORTED-NOT-AVAILABLE]" : local.master_password
+      database = aws_db_instance.mysql.db_name
+
+      connection_string = local.is_db_instance_import ? (
+        length(aws_db_instance.read_replicas) > 0 ?
+        format(
+          "mysql://%s:[PASSWORD]@%s:%d/%s",
+          aws_db_instance.mysql.username,
+          aws_db_instance.read_replicas[0].address,
+          aws_db_instance.read_replicas[0].port,
+          aws_db_instance.mysql.db_name
+        ) :
+        format(
+          "mysql://%s:[PASSWORD]@%s:%d/%s",
+          aws_db_instance.mysql.username,
+          aws_db_instance.mysql.address,
+          aws_db_instance.mysql.port,
+          aws_db_instance.mysql.db_name
+        )
+        ) : (
+        length(aws_db_instance.read_replicas) > 0 ?
+        format(
+          "mysql://%s:%s@%s:%d/%s",
+          aws_db_instance.mysql.username,
+          local.master_password,
+          aws_db_instance.read_replicas[0].address,
+          aws_db_instance.read_replicas[0].port,
+          aws_db_instance.mysql.db_name
+        ) :
+        format(
+          "mysql://%s:%s@%s:%d/%s",
+          aws_db_instance.mysql.username,
+          local.master_password,
+          aws_db_instance.mysql.address,
+          aws_db_instance.mysql.port,
+          aws_db_instance.mysql.db_name
+        )
+      )
     })
+
     writer = sensitive({
       host              = aws_db_instance.mysql.address
       port              = aws_db_instance.mysql.port
