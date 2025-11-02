c46ad941177b0e2011d94f86974be23872a5f8c7
ishaankalra <ishaan.kalra@facets.cloud>
2025-11-02 22:10:13 +0530

    removed environment label

--- common/artifactory/standard/1.0/outputs.tf
+++ common/artifactory/standard/1.0/outputs.tf
@@ -1,6 +1,6 @@
 locals {
   secrets                        = [for key, value in merge(module.name_dockerhub, module.name) : value.name]
-  output_registry_secrets_list   = local.secrets
+  output_registry_secrets_list   = [for value in local.secrets : { name = value }]
   output_registry_secret_objects = { for value in local.secrets : value => [{ name = value }] }
   output_interfaces              = {}
   output_attributes = {
