533915a1e49dbeb1d31e2276682c1d1a450c1735
ishaankalra <ishaan.kalra@facets.cloud>
2025-11-03 11:29:43 +0530

    fix in prometheus

--- common/prometheus/k8s_standard/1.0/main.tf
+++ common/prometheus/k8s_standard/1.0/main.tf
@@ -10,11 +10,11 @@ module "name" {
 
 # Deploy kube-prometheus-stack
 resource "helm_release" "prometheus-operator" {
-  depends_on = [module.prometheus-pvc, module.alertmanager-pvc]
-  wait       = true
-  name       = module.name.name
-  repository = "https://prometheus-community.github.io/helm-charts"
-  # version         = "43.2.1"
+  depends_on      = [module.prometheus-pvc, module.alertmanager-pvc]
+  wait            = true
+  name            = module.name.name
+  repository      = "https://prometheus-community.github.io/helm-charts"
+  version         = "79.1.1"
   chart           = "kube-prometheus-stack"
   namespace       = local.namespace
   cleanup_on_fail = true
@@ -23,17 +23,6 @@ resource "helm_release" "prometheus-operator" {
     # Merge default values with user-provided values and EC2 scrape config if needed
     yamlencode(merge(
       local.default_values,
-      # Add EC2 scrape config if IRSA is enabled
-      local.irsa_config.enabled == 1 ? {
-        prometheus = {
-          prometheusSpec = {
-            additionalScrapeConfigs = concat(
-              local.default_values.prometheus.prometheusSpec.additionalScrapeConfigs,
-              local.ec2_scrape_config
-            )
-          }
-        }
-      } : {},
       # Add storage configuration using the PVC utility module
       {
         prometheus = {
@@ -85,7 +74,6 @@ resource "helm_release" "prometheus-operator" {
         },
       },
       # Add service account config for IRSA if enabled
-      local.service_account_config,
       # Add user-provided values from spec.values
       local.valuesSpec
     ))
@@ -94,12 +82,12 @@ resource "helm_release" "prometheus-operator" {
 
 # Deploy Pushgateway
 resource "helm_release" "prometheus-pushgateway" {
-  depends_on = [helm_release.prometheus-operator]
-  name       = "${module.name.name}-pushgateway"
-  chart      = "prometheus-pushgateway"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  namespace  = local.namespace
-  # version         = "2.0.2"
+  depends_on      = [helm_release.prometheus-operator]
+  name            = "${module.name.name}-pushgateway"
+  chart           = "prometheus-pushgateway"
+  repository      = "https://prometheus-community.github.io/helm-charts"
+  namespace       = local.namespace
+  version         = "3.4.2"
   wait            = true
   cleanup_on_fail = true
 
