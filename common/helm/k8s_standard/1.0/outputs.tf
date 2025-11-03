bb4b62c5d00996c40e3b50fdf6b768b5b7f51fdc
ishaankalra <ishaan.kalra@facets.cloud>
2025-11-03 15:05:41 +0530

    added values

--- common/helm/k8s_standard/1.0/outputs.tf
+++ common/helm/k8s_standard/1.0/outputs.tf
@@ -1,14 +1,7 @@
 locals {
   output_attributes = {
     release_name = helm_release.external_helm_charts.name
-    values       = helm_release.external_helm_charts.metadata[0].values
+    values       = jsondecode(helm_release.external_helm_charts.metadata.values)
   }
   output_interfaces = {}
-}
-
-output "metadata" {
-  value = helm_release.external_helm_charts.metadata
-}
-output "status" {
-  value = helm_release.external_helm_charts.status
 }
