3bbc267a8bedcb9944c386af26689debece802b8
srikxcipher <pandasrikant365@gmail.com>
2025-11-24 16:59:06 +0530

    fix : added nodepool_input and fixed helm_chart configuration

--- common/eck-operator/helm/1.0/main.tf
+++ common/eck-operator/helm/1.0/main.tf
@@ -1,17 +1,23 @@
 # main.tf - ECK Operator Helm Chart Deployment
 
-module "eck_operator" {
-  source = "github.com/Facets-cloud/facets-utility-modules//helm"
-  
+resource "helm_release" "eck_operator" {
   name             = var.instance_name
+  repository       = local.repository
+  chart            = local.chart_name
+  version          = local.chart_version
   namespace        = local.namespace
   create_namespace = local.create_namespace
-  
-  repository = local.repository
-  chart      = local.chart_name
-  version    = local.chart_version
-  
-  values = [yamlencode(local.final_values)]
-  
-  advanced_config = {}
+
+  values = [
+    yamlencode(local.final_values)
+  ]
+
+  # Wait for the operator to be deployed before marking as complete
+  wait          = true
+  wait_for_jobs = true
+  timeout       = 600
+
+  # Cleanup on failure
+  atomic          = true
+  cleanup_on_fail = true
 }
--- common/eck-operator/helm/1.0/outputs.tf
+++ common/eck-operator/helm/1.0/outputs.tf
@@ -1,22 +1,23 @@
 locals {
   output_attributes = {
-    namespace        = local.namespace
-    release_name     = var.instance_name
-    chart_version    = local.chart_version
-    operator_name    = "elastic-operator"
-    repository       = local.repository
-    chart_name       = local.chart_name
-    webhook_enabled  = true
-    managed_namespaces = lookup(local.final_values, "managedNamespaces", ["elastic-system"])
+    namespace       = local.namespace
+    release_name    = helm_release.eck_operator.name
+    chart_version   = helm_release.eck_operator.version
+    operator_name   = "elastic-operator"
+    repository      = local.repository
+    chart_name      = local.chart_name
+    webhook_enabled = true
+    status          = helm_release.eck_operator.status
+    revision        = helm_release.eck_operator.metadata[0].revision
   }
-  
+
   output_interfaces = {
     operator = {
-      namespace        = local.namespace
-      release_name     = var.instance_name
-      operator_name    = "elastic-operator"
-      webhook_service  = "elastic-webhook-server"
-      webhook_port     = 9443
+      namespace       = local.namespace
+      release_name    = var.instance_name
+      operator_name   = "elastic-operator"
+      webhook_service = "elastic-webhook-server"
+      webhook_port    = 9443
     }
     webhook = {
       service = "elastic-webhook-server"
