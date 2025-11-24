6564b59ffb9d1543b6c68a6fb32de9d20632ad10
Srikant Snehit Panda <pandasrikant365@gmail.com>
2025-11-24 13:21:35 +0530

    Fixing output schema across all datastore modules  (#20)
    * fix : corrected output keys to follow unified naming convention.
    
    * backup : commented out output_attributes across all outputs.tf files.
    
    * fix : added output_attributes = {} to avoid release errors.
    
    * backup : commented out rest datastore modules and added output_attributes = {} to avoid release errors.
    
    * fix : removed commented out output_attributes blocks.
    
    * unified output type to follow @facets/<intent> and corrected output schema.
    
    * fix : corrected output type in mysql/aws-rds/1.0/facets.yaml
    
    * fix : fixed ouputs structure and added secrets = [].
    
    * fix - corrected secrets = [] in outputs.tf files.
    
    * fix : removed -test from flavor name.
    
    * fix(auth): correct password reference and fixed connection_string across datastore modules.
    
    * Revert "fix(auth): correct password reference and fixed connection_string across datastore modules."
    
    This reverts commit 413dc9dee8a903bf00fae96fae89c1f62dd75368.

--- datastore/mongo/aws-documentdb/1.0/outputs.tf
+++ datastore/mongo/aws-documentdb/1.0/outputs.tf
@@ -1,40 +1,30 @@
 locals {
-  output_attributes = {
-    cluster_identifier      = aws_docdb_cluster.main.cluster_identifier
-    cluster_endpoint        = local.cluster_endpoint
-    cluster_reader_endpoint = aws_docdb_cluster.main.reader_endpoint
-    port                    = local.cluster_port
-    master_username         = local.master_username
-    master_password         = local.master_password
-    db_subnet_group_name    = local.is_import ? var.instance.spec.imports.subnet_group_name : aws_docdb_subnet_group.main[0].name
-    security_group_id       = local.security_group_id
-    engine_version          = aws_docdb_cluster.main.engine_version
-    backup_retention_period = aws_docdb_cluster.main.backup_retention_period
-    preferred_backup_window = aws_docdb_cluster.main.preferred_backup_window
-    cluster_members         = aws_docdb_cluster.main.cluster_members
-  }
+  output_attributes = {}
   output_interfaces = {
     writer = {
       host              = local.cluster_endpoint
       port              = tostring(local.cluster_port)
       username          = local.master_username
       password          = local.master_password
-      connection_string = local.is_import ? "*** IMPORTED - CONNECTION STRING NOT ACCESSIBLE ***" : local.connection_string
+      connection_string = local.connection_string
       name              = aws_docdb_cluster.main.cluster_identifier
+      secrets           = ["password", "connection_string"]
     }
     reader = {
       host              = aws_docdb_cluster.main.reader_endpoint
       port              = tostring(local.cluster_port)
       username          = local.master_username
       password          = local.master_password
-      connection_string = local.is_import ? "*** IMPORTED - CONNECTION STRING NOT ACCESSIBLE ***" : "mongodb://${local.master_username}:${local.master_password}@${aws_docdb_cluster.main.reader_endpoint}:${local.cluster_port}/?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+      connection_string = "mongodb://${local.master_username}:${local.master_password}@${aws_docdb_cluster.main.reader_endpoint}:${local.cluster_port}/?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
       name              = aws_docdb_cluster.main.cluster_identifier
+      secrets           = ["password", "connection_string"]
     }
     cluster = {
       endpoint          = "${local.cluster_endpoint}:${local.cluster_port}"
       username          = local.master_username
       password          = local.master_password
-      connection_string = local.is_import ? "*** IMPORTED - CONNECTION STRING NOT ACCESSIBLE ***" : local.connection_string
+      connection_string = local.connection_string
+      secrets           = ["password", "connection_string"]
     }
   }
 }
