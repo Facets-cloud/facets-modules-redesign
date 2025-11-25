ec3fd46a8e9c36966a9767acfb8379b3b034384a
srikxcipher <pandasrikant365@gmail.com>
2025-11-25 17:14:33 +0530

    new : added k8s-custom-mongodb-operator for MongoDB module...

---
+++ common/mongodb-operator/helm/1.0/outputs.tf
@@ -0,0 +1,12 @@
+locals {
+  output_attributes = {
+    namespace     = local.namespace
+    release_name  = helm_release.mongodb_operator.name
+    chart_version = helm_release.mongodb_operator.version
+    operator_name = "mongodb-kubernetes-operator"
+    repository    = local.repository
+    chart_name    = local.chart_name
+    status        = helm_release.mongodb_operator.status
+    revision      = helm_release.mongodb_operator.metadata[0].revision
+  }
+}
