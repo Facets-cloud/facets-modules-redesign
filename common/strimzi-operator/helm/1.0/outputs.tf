82ea2bbbe65a8ffdae843fbcfe606ca001860733
Srikant Snehit Panda <pandasrikant365@gmail.com>
2025-11-26 12:15:34 +0530

    k8s custom strimzi-operator module for Kafka (#41)
    * new : added module files for kafka-strimzi-operator...
    
    * added output block for exporting output_attributes
    
    * added output_interfaces = {}

---
+++ common/strimzi-operator/helm/1.0/outputs.tf
@@ -0,0 +1,13 @@
+locals {
+  output_attributes = {
+    namespace     = local.namespace
+    release_name  = helm_release.strimzi_operator.name
+    chart_version = helm_release.strimzi_operator.version
+    operator_name = "strimzi-cluster-operator"
+    repository    = local.repository
+    chart_name    = local.chart_name
+    status        = helm_release.strimzi_operator.status
+    revision      = helm_release.strimzi_operator.metadata[0].revision
+  }
+  output_interfaces = {}
+}
