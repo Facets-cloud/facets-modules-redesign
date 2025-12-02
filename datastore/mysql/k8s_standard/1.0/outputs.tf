39b585967ffbf2eb43cec73872121fb4d15140eb
srikxcipher <pandasrikant365@gmail.com>
2025-12-02 20:41:52 +0530

    fix : updated output type to @facets/mysql.

--- datastore/mysql/k8s_standard/1.0/outputs.tf
+++ datastore/mysql/k8s_standard/1.0/outputs.tf
@@ -1,28 +1,5 @@
 locals {
-  output_attributes = {
-    cluster_name      = local.cluster_name
-    namespace         = local.namespace
-    mysql_version     = var.instance.spec.mysql_version
-    mode              = var.instance.spec.mode
-    replicas          = local.replicas
-    resource_type     = "mysql"
-    resource_name     = var.instance_name
-    primary_service   = try(data.kubernetes_service.mysql_primary.metadata[0].name, "${local.cluster_name}-mysql")
-    read_service      = local.reader_host != null ? try(kubernetes_service.mysql_read[0].metadata[0].name, null) : null
-    connection_secret = try(data.kubernetes_secret.mysql_credentials.metadata[0].name, "${local.cluster_name}-conn-credential")
-    pod_prefix = {
-      writer = "${local.cluster_name}-mysql"
-      reader = local.create_read_service ? "${local.cluster_name}-mysql-read" : null
-    }
-    selectors = {
-      mysql = {
-        "app.kubernetes.io/instance"   = local.cluster_name
-        "app.kubernetes.io/managed-by" = "kubeblocks"
-        "app.kubernetes.io/name"       = "mysql"
-      }
-    }
-    defaultDatabase = local.mysql_database
-  }
+  output_attributes = {}
   output_interfaces = {
     writer = {
       host              = local.writer_host
