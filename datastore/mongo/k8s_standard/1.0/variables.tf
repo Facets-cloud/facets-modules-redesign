bb4a18c759f12032939fb28fdef8ab4b3307f34f
srikxcipher <pandasrikant365@gmail.com>
2025-12-11 11:41:53 +0530

    fix : removed non-required vars for nodepool_input.

--- datastore/mongo/k8s_standard/1.0/variables.tf
+++ datastore/mongo/k8s_standard/1.0/variables.tf
@@ -55,8 +55,6 @@ variable "inputs" {
       attributes = object({
         node_pool_name = string
         node_pool_id   = string
-        disk_size_gb   = number
-        node_count     = number
 
         # List of taint objects: { key, value, effect }
         taints = optional(list(object({
