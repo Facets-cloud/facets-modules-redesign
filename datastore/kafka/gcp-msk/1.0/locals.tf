3533e7f8d3ca9752d3d178a03c0cf4099babceff
Ishaan Kalra <108509616+ishaankalra@users.noreply.github.com>
2025-12-12 08:55:00 +0530

    GCP MSK KAFKA (#54)
    * initial commit
    
    * removed replication_config
    
    * facets.yaml indentation
    
    * network variable fixes
    
    * passing google beta provider
    
    * added permission for grant permission roles/cloudkms.cryptoKeyEncrypterDecrypter
    
    * fix : modified the output_type and updated outputs.tf to contain only required attributes.
    
    * new : added module files for kafka-topic.
    
    * fix - cluster and location reference got updated.
    
    * fix : removed topic_id from yaml now defaults to var.instance_name and removed provider from kafka_cluster input
    
    * fix : updated yaml sample and corrected x-ui-yaml-editor in configs_
    
    * fix : added connect_cluster.tf and modified configs to support connect_cluster resource.
    
    * new : added module files for kafka-connector.
    
    * added README.md for kafka (gcp-msk) module and modified readme of kafka-topic to have updated output type.
    
    * renamed
    
    * renmoced connector id
    
    * readme fix - kafka_topic.
    
    * removed kafka_connector module.
    
    * fix : modified connect_cluster field to object in yaml and added separate resource configurations for connect_cluster.
    
    * fix : removed x-ui-visible-if from yaml.
    
    * added x-ui-visible if condition
    
    ---------
    
    Co-authored-by: srikxcipher <pandasrikant365@gmail.com>

---
+++ datastore/kafka/gcp-msk/1.0/locals.tf
@@ -0,0 +1,26 @@
+# Local variables for main.tf configuration only
+# Output locals are handled in outputs.tf by the Facets framework
+
+locals {
+  cluster_name = "${var.instance_name}-${var.environment.unique_name}"
+
+  # Kafka configuration
+  kafka_version = var.instance.spec.version_config.kafka_version
+  vcpu_count    = var.instance.spec.sizing.vcpu_count
+  memory_bytes  = var.instance.spec.sizing.memory_gb * 1024 * 1024 * 1024
+  disk_size_gb  = var.instance.spec.sizing.disk_size_gb
+
+  # Region and project from inputs
+  project_id = var.inputs.gcp_cloud_account.attributes.project_id
+  region     = var.inputs.gcp_cloud_account.attributes.region
+
+  # Common labels
+  common_labels = merge(
+    var.environment.cloud_tags,
+    {
+      name        = local.cluster_name
+      environment = var.environment.name
+      module      = "kafka-gcp-msk"
+    }
+  )
+}
