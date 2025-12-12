fcd45ff85ba1ecd0c5e0abc9b8409427ac35a2c5
ishaankalra <ishaan.kalra@facets.cloud>
2025-12-12 15:37:41 +0530

    removed interfaces and updated attributes

--- modules/common/kubeblocks-operator/standard/1.0/outputs.tf
+++ modules/common/kubeblocks-operator/standard/1.0/outputs.tf
@@ -4,21 +4,8 @@ locals {
     version        = helm_release.kubeblocks.version
     chart_version  = helm_release.kubeblocks.version
     release_name   = helm_release.kubeblocks.name
-    release_status = helm_release.kubeblocks.status
-    crd_dependency = local.crd_release_id
+    release_id     = helm_release.kubeblocks.id
   }
 
-  output_interfaces = {
-    output = {
-      # Helm release ID for dependency management (known after apply)
-      release_id     = helm_release.kubeblocks.id
-      release_status = helm_release.kubeblocks.status
-
-      # Readiness indicator - use status directly instead of boolean expression
-      ready = helm_release.kubeblocks.status
-
-      # Dependency key for other modules to wait on
-      dependency_id = "${helm_release.kubeblocks.id}-${kubernetes_namespace.kubeblocks.metadata[0].uid}"
-    }
-  }
+  output_interfaces = {}
 }
