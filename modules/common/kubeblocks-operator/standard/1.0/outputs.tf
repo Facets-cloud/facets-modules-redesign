91155d78b84579395c31e1a801efc3f1ae7c707d
ishaankalra <ishaan.kalra@facets.cloud>
2025-12-12 15:38:19 +0530

    fmt

--- modules/common/kubeblocks-operator/standard/1.0/outputs.tf
+++ modules/common/kubeblocks-operator/standard/1.0/outputs.tf
@@ -1,10 +1,10 @@
 locals {
   output_attributes = {
-    namespace      = kubernetes_namespace.kubeblocks.metadata[0].name
-    version        = helm_release.kubeblocks.version
-    chart_version  = helm_release.kubeblocks.version
-    release_name   = helm_release.kubeblocks.name
-    release_id     = helm_release.kubeblocks.id
+    namespace     = kubernetes_namespace.kubeblocks.metadata[0].name
+    version       = helm_release.kubeblocks.version
+    chart_version = helm_release.kubeblocks.version
+    release_name  = helm_release.kubeblocks.name
+    release_id    = helm_release.kubeblocks.id
   }
 
   output_interfaces = {}
