82d7db023e6727dc102c2a35a5befef7943b3a22
srikantpanda <srikant.panda@facets.cloud>
2025-12-27 06:05:55 +0530

    added namespace in outputs.

--- modules/common/prometheus/k8s_standard/1.0/outputs.tf
+++ modules/common/prometheus/k8s_standard/1.0/outputs.tf
@@ -1,6 +1,7 @@
 locals {
   output_interfaces = {}
   output_attributes = {
+    namespace            = local.namespace
     prometheus_url       = "http://${module.name.name}.${var.environment.namespace}.svc.cluster.local:9090"
     alertmanager_url     = "http://${module.name.name}-alertmanager.${var.environment.namespace}.svc.cluster.local:9093"
     grafana_url          = "http://${module.name.name}-grafana.${var.environment.namespace}.svc.cluster.local:80"
