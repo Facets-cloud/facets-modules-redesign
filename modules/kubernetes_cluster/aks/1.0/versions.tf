6ece20f679fa8909996ff8b2927a2c0dd1c5b584
omendra-tomar <omendra.tomar@facets.cloud>
2026-01-06 12:58:01 +0530

    reverted some changes here

--- modules/kubernetes_cluster/aks/1.0/versions.tf
+++ modules/kubernetes_cluster/aks/1.0/versions.tf
@@ -1,2 +1,7 @@
 terraform {
+  required_providers {
+    azapi = {
+      source = "Azure/azapi"
+    }
+  }
 }
--- modules/service/azure/1.0/version.tf
+++ modules/service/azure/1.0/version.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.0"
   required_providers {
     facets = {
       source  = "Facets-cloud/facets"
       version = "0.1.3"
     }
   }
+  required_version = ">= 1.0"
 }
