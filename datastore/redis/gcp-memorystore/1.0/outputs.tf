966d15dc4efe5ac458de3066c47ee18c4b746b03
srikantpanda <srikant.panda@facets.cloud>
2026-01-09 12:39:57 +0530

    updated outputs

--- datastore/redis/gcp-memorystore/1.0/outputs.tf
+++ datastore/redis/gcp-memorystore/1.0/outputs.tf
@@ -1,24 +1,7 @@
 locals {
   output_attributes = {
-    instance_id             = google_redis_instance.main.id
-    instance_name           = google_redis_instance.main.name
-    host                    = google_redis_instance.main.host
-    port                    = google_redis_instance.main.port
-    redis_version           = google_redis_instance.main.redis_version
-    tier                    = google_redis_instance.main.tier
-    memory_size_gb          = google_redis_instance.main.memory_size_gb
-    region                  = local.region
-    current_location_id     = google_redis_instance.main.current_location_id
-    read_endpoint           = google_redis_instance.main.read_endpoint
-    read_endpoint_port      = google_redis_instance.main.read_endpoint_port
-    authorized_network      = google_redis_instance.main.authorized_network
-    connect_mode            = google_redis_instance.main.connect_mode
-    create_time             = google_redis_instance.main.create_time
-    transit_encryption_mode = google_redis_instance.main.transit_encryption_mode
-    tls_enabled             = local.enable_tls
     server_ca_certs         = sensitive(local.enable_tls ? google_redis_instance.main.server_ca_certs : [])
-    auth_string             = sensitive(google_redis_instance.main.auth_string)
-    secrets                 = ["server_ca_certs", "auth_string"]
+    secrets                 = ["server_ca_certs"]
   }
   output_interfaces = {
     cluster = {
@@ -32,8 +15,6 @@ locals {
         google_redis_instance.main.host,
         google_redis_instance.main.port
       )
-      tls_enabled     = local.enable_tls
-      server_ca_certs = local.enable_tls ? google_redis_instance.main.server_ca_certs : []
       secrets         = ["auth_token", "connection_string", "server_ca_certs"]
     }
   }
