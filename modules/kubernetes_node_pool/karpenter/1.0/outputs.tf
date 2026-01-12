0759d41aa5e8ecda8592a1bb7a1012df34d1b6a7
Shrinidhi-59 <shrinidhi.p@facets.cloud>
2026-01-12 15:38:50 +0530

    added a karpenter module

--- modules/kubernetes_node_pool/karpenter/1.0/outputs.tf
+++ modules/kubernetes_node_pool/karpenter/1.0/outputs.tf
@@ -1,23 +1,7 @@
 locals {
   output_attributes = {
-    # Karpenter controller details
-    karpenter_namespace       = local.karpenter_namespace
-    karpenter_service_account = local.karpenter_service_account
-    karpenter_version         = var.instance.spec.karpenter_version
-
-    # IAM details - use created resources when install_karpenter is true, otherwise use input values
-    controller_role_arn        = local.install_karpenter ? aws_iam_role.karpenter_controller[0].arn : try(var.inputs.karpenter_details.attributes.controller_role_arn, "")
-    node_role_arn              = local.node_role_arn
-    node_instance_profile_name = local.node_instance_profile_name
-
-    # Interruption handling
-    interruption_queue_name = local.install_karpenter && var.instance.spec.interruption_handling ? aws_sqs_queue.karpenter_interruption[0].name : ""
-
-    # NodePool details for this instance
-    nodepool_name  = "${var.instance_name}-nodepool"
-    nodeclass_name = "${var.instance_name}-nodeclass"
-
-    # Taints configured for this node pool (use this in your service/deployment tolerations)
+    node_pool_name  = "${var.instance_name}-nodepool"
+    node_class_name = "${var.instance_name}-nodeclass"
     taints = [
       for taint_key, taint_config in lookup(var.instance.spec, "taints", {}) : {
         key    = taint_key
@@ -25,12 +9,7 @@ locals {
         effect = taint_config.effect
       }
     ]
-
-    # Labels configured for this node pool (use this in your service/deployment nodeSelectors)
-    labels = lookup(var.instance.spec, "labels", {})
-
-    secrets = []
+    node_selector = lookup(var.instance.spec, "labels", {})
   }
-
   output_interfaces = {}
 }
