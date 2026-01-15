8e4309e4e12a6d3922e7f25c940126af1aa8c284
omendra-tomar <omendra.tomar@facets.cloud>
2026-01-15 17:14:02 +0530

    aws external-dns

--- modules/external_dns/aws/1.0/outputs.tf
+++ modules/external_dns/aws/1.0/outputs.tf
@@ -1,8 +1,10 @@
 locals {
   output_attributes = {
     # Secret information (created by this module)
-    secret_name      = kubernetes_secret.external_dns_r53_secret.metadata[0].name
-    secret_namespace = local.namespace
+    # Note: Secret is created in BOTH external-dns and cert-manager namespaces
+    # Output points to cert-manager namespace since that's what cert-manager module expects
+    secret_name      = kubernetes_secret.cert_manager_r53_secret.metadata[0].name
+    secret_namespace = local.cert_manager_namespace
 
     # AWS credential keys (for cert-manager integration)
     aws_access_key_id_key     = "access-key-id"
