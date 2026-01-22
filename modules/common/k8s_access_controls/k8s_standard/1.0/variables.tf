adf3b6926541612462bb2dd45e47178290835247
ishaankalra <ishaan.kalra@facets.cloud>
2026-01-22 07:52:05 +0530

    fixed facets.yaml and fixed variables

--- modules/common/k8s_access_controls/k8s_standard/1.0/variables.tf
+++ modules/common/k8s_access_controls/k8s_standard/1.0/variables.tf
@@ -1,38 +1,60 @@
-variable "cluster" {
-  type = any
-  default = {
-  }
-}
-
-variable "baseinfra" {
-  type = any
-  default = {
-    k8s_details = {
-      registry_secret_objects = []
-    }
-  }
-}
-
-variable "cc_metadata" {
-  type = any
-  default = {
-    tenant_base_domain : "tenant.facets.cloud"
-  }
-}
-
 variable "instance" {
-  type = any
+  description = "Resource instance configuration from blueprint"
+  type = object({
+    kind    = string
+    flavor  = string
+    version = string
+    spec = object({
+      roles = optional(map(object({
+        metadata = optional(object({
+          namespace   = optional(string)
+          annotations = optional(map(string), {})
+          labels      = optional(map(string), {})
+        }))
+        rules = map(object({
+          api_groups     = list(string)
+          resources      = list(string)
+          resource_names = optional(list(string))
+          verbs          = list(string)
+        }))
+      })), {})
+      cluster_roles = optional(map(object({
+        metadata = optional(object({
+          annotations = optional(map(string), {})
+          labels      = optional(map(string), {})
+        }))
+        rules = map(object({
+          api_groups        = optional(list(string))
+          resources         = optional(list(string))
+          resource_names    = optional(list(string))
+          non_resource_urls = optional(list(string))
+          verbs             = list(string)
+        }))
+      })), {})
+    })
+  })
 }
-
 
 variable "instance_name" {
-  type    = string
-  default = "test_instance"
+  description = "Unique architectural name from blueprint"
+  type        = string
 }
 
 variable "environment" {
-  type = any
-  default = {
-    namespace = "default"
-  }
+  description = "Environment context"
+  type = object({
+    name        = string
+    unique_name = optional(string)
+    namespace   = optional(string, "default")
+    cloud_tags  = optional(map(string), {})
+  })
+}
+
+variable "inputs" {
+  description = "Input references from other modules"
+  type = object({
+    kubernetes_details = object({
+      attributes = any
+    })
+  })
 }
