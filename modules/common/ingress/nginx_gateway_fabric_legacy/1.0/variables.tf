097d6c61548aa41192f9a6f8463bca666c7defa0
Sanmesh Kakade <sanmeshkakade@Sanmeshs-MacBook-Air.local>
2026-01-27 17:09:07 +0530

    legacy version of nginx fabric gateway ingress

---
+++ modules/common/ingress/nginx_gateway_fabric_legacy/1.0/variables.tf
@@ -0,0 +1,37 @@
+variable "cluster" {
+  type    = any
+  default = {}
+}
+
+variable "baseinfra" {
+  type    = any
+  default = {}
+}
+
+variable "cc_metadata" {
+  type    = any
+  default = {
+    tenant_base_domain = "tenant.facets.cloud"
+  }
+}
+
+variable "instance" {
+  type = any
+}
+
+variable "instance_name" {
+  type    = string
+  default = "test_instance"
+}
+
+variable "environment" {
+  type = any
+  default = {
+    namespace = "default"
+  }
+}
+
+variable "inputs" {
+  type = any
+  default = {}
+}
