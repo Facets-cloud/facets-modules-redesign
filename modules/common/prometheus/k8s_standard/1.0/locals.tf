d71af5eecae25fe5dee00aadd04c6f2a3f5d14e1
srikantpanda <srikant.panda@facets.cloud>
2026-01-27 12:31:12 +0530

    fix : Helm values merge order for Alertmanager config with Facets webhook - [MUST NEVER BE OVERRIDDEN]

--- modules/common/prometheus/k8s_standard/1.0/locals.tf
+++ modules/common/prometheus/k8s_standard/1.0/locals.tf
@@ -87,6 +87,43 @@ locals {
 
   prometheus_retention = lookup(local.spec, "retention", "100d")
 
+  # Alertmanager config with Facets webhook - MUST NEVER BE OVERRIDDEN
+  alertmanager_config = {
+    alertmanager = {
+      config = {
+        global = {
+          resolve_timeout = "60m"
+        }
+        route = {
+          receiver        = "default"
+          group_by        = ["alertname", "entity"]
+          routes          = []
+          group_wait      = "30s"
+          group_interval  = "5m"
+          repeat_interval = "6h"
+        }
+        receivers = [
+          {
+            name = "default"
+            webhook_configs = [
+              {
+                url           = "http://alertmanager-webhook.default/alerts"
+                send_resolved = true
+              },
+              {
+                url           = "https://${var.cc_metadata.cc_host}/cc/v1/clusters/${var.environment.cloud_tags.facetsclusterid}/alerts"
+                send_resolved = true
+                http_config = {
+                  bearer_token = var.cc_metadata.cc_auth_token
+                }
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
+
   # Nodepool configuration from inputs (encode/decode pattern)
   # Decode back into object
   nodepool_config      = lookup(var.inputs, "kubernetes_node_pool_details", null)
@@ -219,37 +256,6 @@ locals {
         nodeSelector = local.nodeSelector
         tolerations  = local.tolerations
         # priorityClassName = "facets-critical"
-      }
-      config = {
-        global = {
-          resolve_timeout = "60m"
-        }
-        route = {
-          receiver        = "default"
-          group_by        = ["alertname", "entity"]
-          routes          = []
-          group_wait      = "30s"
-          group_interval  = "5m"
-          repeat_interval = "6h"
-        }
-        receivers = [
-          {
-            name = "default"
-            webhook_configs = [
-              {
-                url           = "http://alertmanager-webhook.default/alerts"
-                send_resolved = true
-              },
-              {
-                url           = "https://${var.cc_metadata.cc_host}/cc/v1/clusters/${var.environment.cloud_tags.facetsclusterid}/alerts"
-                send_resolved = true
-                http_config = {
-                  bearer_token = var.cc_metadata.cc_auth_token
-                }
-              }
-            ]
-          }
-        ]
       }
     }
     grafana = {
