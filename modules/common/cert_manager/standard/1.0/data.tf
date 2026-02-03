ea4608328b44f6000f13f76977f1a3d13a9442a4
omendra-tomar <omendra.tomar@facets.cloud>
2026-02-03 14:36:18 +0530

    added data.aws_route53_zone and read from this TF_VAR_tenant_base_domain

--- modules/common/cert_manager/standard/1.0/data.tf
+++ modules/common/cert_manager/standard/1.0/data.tf
@@ -2,14 +2,16 @@
 # This replaces the deprecated var.cc_metadata and var.cluster pattern
 data "external" "cc_env" {
   program = ["sh", "-c", <<-EOT
-    echo "{\"cc_tenant_provider\":\"$TF_VAR_cc_tenant_provider\",\"cc_region\":\"$TF_VAR_cc_region\",\"tenant_base_domain_id\":\"$TF_VAR_tenant_base_domain_id\"}"
+    echo "{\"cc_tenant_provider\":\"$TF_VAR_cc_tenant_provider\",\"cc_region\":\"$TF_VAR_cc_region\",\"tenant_base_domain\":\"$TF_VAR_tenant_base_domain\"}"
   EOT
   ]
 }
 
 locals {
   # Control plane metadata from environment variables
-  cc_tenant_provider    = data.external.cc_env.result.cc_tenant_provider
-  cc_region             = data.external.cc_env.result.cc_region
-  tenant_base_domain_id = data.external.cc_env.result.tenant_base_domain_id
+  cc_tenant_provider = data.external.cc_env.result.cc_tenant_provider
+  cc_region          = data.external.cc_env.result.cc_region
+  tenant_base_domain = data.external.cc_env.result.tenant_base_domain
+  # Get zone_id from Route53 data source (queried by domain name)
+  tenant_base_domain_id = length(data.aws_route53_zone.base-domain-zone) > 0 ? data.aws_route53_zone.base-domain-zone[0].zone_id : ""
 }
