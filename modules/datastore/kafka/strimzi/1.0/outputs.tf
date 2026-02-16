4d75936c625eb17e874f7a8d03c744e46eb0a9a8
srikantpanda <srikant.panda@facets.cloud>
2026-02-16 19:14:25 +0530

    removed unwanted attr from outputs and updated nodepool -> optional : false

--- modules/datastore/kafka/strimzi/1.0/outputs.tf
+++ modules/datastore/kafka/strimzi/1.0/outputs.tf
@@ -3,17 +3,8 @@ locals {
   scram_username = "${var.instance_name}-${local.admin_username}"
 
   output_attributes = {
-    namespace         = local.namespace
     cluster_name      = var.instance_name
-    bootstrap_service = local.bootstrap_service
-    bootstrap_servers = "${local.bootstrap_service}.${local.namespace}.svc.cluster.local:9092"
-    replica_count     = local.replica_count
-    broker_endpoints  = local.broker_endpoints
     kafka_version     = local.kafka_version
-    admin_username    = local.scram_username
-    admin_password    = random_password.kafka_admin_password.result
-    ca_cert_secret    = "${var.instance_name}-cluster-ca-cert"
-    secrets           = ["bootstrap_servers", "admin_password", "broker_endpoints"]
   }
 
   output_interfaces = {
