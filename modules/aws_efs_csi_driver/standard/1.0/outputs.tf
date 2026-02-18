3fff9b10fcb9648b73e61810b1e274c318ad8e09
Rauneet Verma <rauneetverma@MacBookAir.lan>
2026-02-18 23:05:20 +0530

    fixed outputs

--- modules/aws_efs_csi_driver/standard/1.0/outputs.tf
+++ modules/aws_efs_csi_driver/standard/1.0/outputs.tf
@@ -2,7 +2,7 @@ locals {
   output_attributes = {
     efs_file_system_id  = aws_efs_file_system.efs_csi_driver.id
     efs_file_system_arn = aws_efs_file_system.efs_csi_driver.arn
-    iam_role_arn        = aws_iam_role.efs_csi_driver.arn
+    iam_role_arn        = module.irsa.iam_role_arn
     storage_class_name  = "efs-sc"
     helm_release_id     = helm_release.efs_csi_driver.id
     secrets             = []
@@ -11,9 +11,3 @@ locals {
   output_interfaces = {}
 }
 
-output "default" {
-  value = {
-    attributes = local.output_attributes
-    interfaces = local.output_interfaces
-  }
-}
