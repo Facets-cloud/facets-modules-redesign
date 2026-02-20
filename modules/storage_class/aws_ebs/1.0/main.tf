5b1a522c8afa755d65c28ef926f026e14dbb53a8
srikantpanda <srikant.panda@facets.cloud>
2026-02-20 13:08:47 +0530

    added base template config for sc

--- modules/storage_class/aws_ebs/1.0/main.tf
+++ modules/storage_class/aws_ebs/1.0/main.tf
@@ -15,22 +15,22 @@ resource "kubernetes_storage_class_v1" "storage_class" {
   }
 
   storage_provisioner    = "ebs.csi.aws.com"
-  reclaim_policy        = var.instance.spec.reclaim_policy
-  volume_binding_mode   = var.instance.spec.volume_binding_mode
-  allow_volume_expansion = var.instance.spec.allow_volume_expansion
+  reclaim_policy         = lookup(var.instance.spec, "reclaim_policy", "Delete")
+  volume_binding_mode    = lookup(var.instance.spec, "volume_binding_mode", "WaitForFirstConsumer")
+  allow_volume_expansion = lookup(var.instance.spec, "allow_volume_expansion", true)
 
   # Build parameters dynamically based on volume type
   parameters = merge(
     {
       type      = var.instance.spec.volume_type
-      encrypted = tostring(var.instance.spec.encrypted)
+      encrypted = tostring(lookup(var.instance.spec, "encrypted", true))
     },
     # Add iops for io1/io2 volumes if specified
-    var.instance.spec.iops != null && contains(["io1", "io2"], var.instance.spec.volume_type) ? {
+    try(var.instance.spec.iops, null) != null && contains(["io1", "io2"], var.instance.spec.volume_type) ? {
       iops = tostring(var.instance.spec.iops)
     } : {},
     # Add throughput for gp3 volumes if specified
-    var.instance.spec.throughput != null && var.instance.spec.volume_type == "gp3" ? {
+    try(var.instance.spec.throughput, null) != null && var.instance.spec.volume_type == "gp3" ? {
       throughput = tostring(var.instance.spec.throughput)
     } : {}
   )
--- modules/storage_class/aws_ebs/1.0/variables.tf
+++ modules/storage_class/aws_ebs/1.0/variables.tf
@@ -1,15 +1,15 @@
 variable "environment" {
   description = "Environment details"
   type = object({
-    name                 = string
-    unique_name          = string
-    namespace            = string
-    environment_id       = string
-    cloud_provider       = string
-    cloud_provider_id    = string
-    cloud_region         = string
+    name                     = string
+    unique_name              = string
+    namespace                = string
+    environment_id           = string
+    cloud_provider           = string
+    cloud_provider_id        = string
+    cloud_region             = string
     cloud_availability_zones = list(string)
-    cloud_tags           = map(string)
+    cloud_tags               = map(string)
   })
 }
 
@@ -21,14 +21,14 @@ variable "instance" {
     version  = string
     metadata = map(string)
     spec = object({
-      name                  = string
-      volume_type           = string
-      is_default            = bool
-      iops                  = optional(number)
-      throughput            = optional(number)
-      encrypted             = optional(bool, true)
-      reclaim_policy        = optional(string, "Delete")
-      volume_binding_mode   = optional(string, "WaitForFirstConsumer")
+      name                   = string
+      volume_type            = string
+      is_default             = bool
+      iops                   = optional(number)
+      throughput             = optional(number)
+      encrypted              = optional(bool, true)
+      reclaim_policy         = optional(string, "Delete")
+      volume_binding_mode    = optional(string, "WaitForFirstConsumer")
       allow_volume_expansion = optional(bool, true)
     })
   })
