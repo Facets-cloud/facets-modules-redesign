b823ea52f9374ac9c577806869d8ae3ad1f28ee0
Rauneet Verma <rauneetverma@Rauneets-MacBook-Air.local>
2026-04-02 11:44:56 +0530

    updated outputs and output.yaml

--- modules/snapscheduler/standard/1.0/outputs.tf
+++ modules/snapscheduler/standard/1.0/outputs.tf
@@ -1,10 +1,11 @@
 locals {
   output_attributes = {
-    release_name = helm_release.snapschedule.name
-    namespace    = helm_release.snapschedule.namespace
-    chart        = helm_release.snapschedule.chart
-    version      = helm_release.snapschedule.version
-    status       = helm_release.snapschedule.status
+    namespace     = helm_release.snapschedule.namespace
+    release_name  = helm_release.snapschedule.name
+    chart         = helm_release.snapschedule.chart
+    version       = helm_release.snapschedule.version
+    status        = helm_release.snapschedule.status
+    operator_name = "snapscheduler"
   }
 
   output_interfaces = {}
