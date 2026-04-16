6e0a02924a4a69b5be24fd997ca9b140fd1875ea
Sanmesh Kakade <sanmesh.kakade@facets.cloud>
2026-04-16 10:39:30 +0530

    wip: update the cloud account scripts so that it works with new iac

--- modules/cloud_account/gcp_provider/1.0/locals.tf
+++ modules/cloud_account/gcp_provider/1.0/locals.tf
@@ -1,7 +1,7 @@
 data "external" "gcp_fetch_cloud_secret" {
   program = [
     "python3",
-    "/sources/primary/capillary-cloud-tf/tfmain/scripts/cloudaccount-fetch-secret/secret-fetcher.py",
+    "${path.module}/secret-fetcher.py",
     var.instance.spec.cloud_account,
     "GCP"
   ]
